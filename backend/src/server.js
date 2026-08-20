801f96cf8b9af72849dd438da474823022a47541
Ilham Maulana <97593153+ihmham@users.noreply.github.com>
2026-08-20 12:22:26 +0700

    Delete backend directory

--- backend/src/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-const app = require('./app')
-
-const port = Number(process.env.PORT) || 3000
-
-app.listen(port, () => {
-  console.log(`Jasatitip API berjalan di http://localhost:${port}`)
-})
